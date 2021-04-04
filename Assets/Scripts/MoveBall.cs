221dd026d5c3df600b6b158aa49fc0159b0eef49
WhiteLyricist <57936182+WhiteLyricist@users.noreply.github.com>
2021-04-04 09:16:56 +0300

    v 1.1

---
+++ Assets/Scripts/MoveBall.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoveBall : MonoBehaviour
+{
+
+    public static Action<string> OnScore = delegate { };
+
+    private float deltaX;
+    private float deltaY;
+
+    private float speed;
+
+    private Vector3 InitialVector;
+
+    private void Awake()
+    {
+        deltaX = UnityEngine.Random.Range(-1.0f, 1.0f);
+        deltaY = UnityEngine.Random.Range(-1.0f, 1.0f);
+        speed = 3.0f;
+
+    }
+
+    void Update()
+    {
+           InitialVector = new Vector3(transform.position.x + deltaX * speed * Time.deltaTime / Mathf.Abs(deltaX), transform.position.y + deltaY * speed * Time.deltaTime / Mathf.Abs(deltaY), -1f);
+           transform.position = InitialVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            deltaX = -deltaX;
+            return;
+        }
+
+        if (collision.gameObject.tag == "Racquet") 
+        {
+            deltaY = -deltaY;
+            return;
+        }
+
+        if (collision.gameObject.tag == "TopWall") 
+        {
+            OnScore("Top");
+            transform.position = new Vector3(0, 0, -1f);
+            deltaX = UnityEngine.Random.Range(-1.0f, 1.0f);
+            deltaY = UnityEngine.Random.Range(-1.0f, 1.0f);
+        }
+
+        if (collision.gameObject.tag == "BotWall") 
+        {
+            OnScore("Bot");
+            transform.position = new Vector3(0, 0, -1f);
+            deltaX = UnityEngine.Random.Range(-1.0f, 1.0f);
+            deltaY = UnityEngine.Random.Range(-1.0f, 1.0f);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+
+    private int scoreBot = 0;
+    private int scoreTop = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveBall.OnScore += Score;
+    }
+
+    void Score(string side) 
+    {
+        if (side == "Bot") 
+        {
+            scoreBot += 1;
+        }
+
+        if (side == "Top") 
+        {
+            scoreTop += 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        MoveBall.OnScore -= Score;
+    }
+
+}
